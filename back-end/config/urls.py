ec6840f6c01a0dbddfbc9976d86685ba6bc0495a
Nguyen Anh Tuan <tuan@samsung.com>
2025-12-02 22:57:18 +0700

    add

--- back-end/config/urls.py
+++ back-end/config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
@@ -23,11 +23,12 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    # Favicon route
-    path('favicon.ico', TemplateView.as_view(template_name='index.html'), name='favicon'),
-    # Serve React app for all non-API routes
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
 
 # Serve static files
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Serve React build index.html for all other routes (SPA routing)
+urlpatterns += [
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html'), name='react_app'),
+]
